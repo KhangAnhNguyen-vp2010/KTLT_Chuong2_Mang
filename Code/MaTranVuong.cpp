99d1afc7546fe7fdd7a5bbacee355da386db8c42
Anh Khang <taicuoi664@gmail.com>
2024-08-04 17:23:55 +0700

    Add MaTranVuong_Bai02_TongCacPhanTuNamTrenDuongCheoPhu

--- Code/MaTranVuong.cpp
+++ Code/MaTranVuong.cpp
@@ -37,6 +37,15 @@ int sum_main_diagonal(int matrix[][MAXSIZE], int n) {
     return sum;
 }
 
+// Hàm tính tổng các phần tử nằm trên đường chéo phụ
+int sum_secondary_diagonal(int matrix[][MAXSIZE], int n) {
+    int sum = 0;
+	for (int i = 0; i < n; i++) {
+        sum += matrix[i][n - i - 1];
+    }
+    return sum;
+}
+
 int main()
 {
 	int a[MAXSIZE][MAXSIZE];
@@ -45,6 +54,8 @@ int main()
 	xuat_MaTranVuong(a,n);
 	printf("\n--------------------------------------\n");
 	printf("Tong cac phan tu nam tren duong cheo chinh la: %d", sum_main_diagonal(a,n));
+	printf("\n--------------------------------------\n");
+	printf("Tong cac phan tu nam tren duong cheo phu la: %d", sum_secondary_diagonal(a,n));
 	getch();
 	return 0;
 }
