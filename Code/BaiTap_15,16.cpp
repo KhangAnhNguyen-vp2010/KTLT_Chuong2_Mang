05bd037288448fa42e979a8e5859cdd8ceee61ea
Le Tan Phu <phule13112004@gmail.com>
2024-08-05 14:39:41 +0700

    Add Bai2_15 ket qua va debug

--- Code/BaiTap_15,16.cpp
+++ Code/BaiTap_15,16.cpp
@@ -18,8 +18,18 @@ void XuatM(int a[], int n) {
 		printf("%d\t", a[i]);
 	}
 }
+void TimMaxMin(int a[], int n) {
+	int max = a[0], min = a[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > max) max = a[i];
+		if (a[i] < min) min = a[i];
+	}
+	printf("\nMax:%d\tMin:%d", max, min);
+}
 int main() {
 	int a[100], n;
 	NhapM(a, n);
 	XuatM(a, n);
+	TimMaxMin(a, n);
 }
