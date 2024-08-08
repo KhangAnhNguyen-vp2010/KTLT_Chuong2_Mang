4855eece180bd1c3c6dcb650cd1714aea99e4743
Le Tan Phu <phule13112004@gmail.com>
2024-08-08 14:16:52 +0700

    Add Bai2_58.cpp KQ.PNG Debug.PNG

--- Code/BT_58.cpp
+++ Code/BT_58.cpp
@@ -33,9 +33,27 @@ void DemSNT(int a[], int n) {
 		if (KTSNT(a[i]) == 1) printf("%d\t", a[i]);
 	}
 }
+int KTToanLe(int x) {
+	while (x > 10) {
+		int temp = x % 10;
+		if (temp % 2 == 0) {
+			return 0;
+		}
+		x /= 10;
+	}
+	return 1;
+}
+void XuatPTToanLe(int a[], int n) {
+	printf("\nCac phan tu toan le\n");
+	for (int i = 0; i < n; i++)
+	{
+		if (KTToanLe(a[i]) == 1) printf("%d\t", a[i]);
+	}
+}
 int main() {
 	int a[100], n;
 	NhapM(a, n);
 	XuatM(a, n);
 	DemSNT(a, n);
+	XuatPTToanLe(a, n);
 }
